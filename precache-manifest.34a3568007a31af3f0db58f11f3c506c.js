cc19357b0200ab1ecf1f24153331193393d4ca47
Hannes Oldenburg <hannes.christian.oldenburg@gmail.com>
2019-01-23 23:45:31 +0000

    Updates

---
+++ precache-manifest.34a3568007a31af3f0db58f11f3c506c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8f6e7b273a424e7e4a73",
+    "url": "/loern/static/css/main.00736d0e.chunk.css"
+  },
+  {
+    "revision": "8f6e7b273a424e7e4a73",
+    "url": "/loern/static/js/main.8f6e7b27.chunk.js"
+  },
+  {
+    "revision": "95f303d5ef010297bbdf",
+    "url": "/loern/static/js/1.95f303d5.chunk.js"
+  },
+  {
+    "revision": "aa5ea3e4bd23291725e6",
+    "url": "/loern/static/js/runtime~main.aa5ea3e4.js"
+  },
+  {
+    "revision": "20e2e6d13438a9bb6b20dc63f8fd4bae",
+    "url": "/loern/index.html"
+  }
+];
